b6848114cf7e94e0e8a5741563af0ca65d569c5d
leonDev06 <leonneldimatulac@gmail.com>
2022-10-28 14:11:44 +0800

    Initial Commit; Added Solutin and WebApi; Create UserController

---
+++ src/Sidekick.Training.WebApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sidekick.Training.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task GetUserById()
+        {
+            Console.WriteLine("GetUserById");
+        }
+
+        [HttpPost]
+        public async Task CreateUserById()
+        {
+            Console.WriteLine("CreateUser");
+        }
+
+        [HttpPut]
+        public async Task UpdateUserById()
+        {
+            Console.WriteLine("UpdateUserById");
+        }
+
+        [HttpDelete]
+        public async Task DeleteUserById()
+        {
+            Console.WriteLine("DeleteUserById");
+        }
+    }
+}
