142ed7ed0aa21bbbefce707f325a7c352aa5ba0d
leonDev06 <leonneldimatulac@gmail.com>
2022-10-31 11:12:44 +0800

    Added interface and implementation for user provide

---
+++ src/Sidekick.Training.Providers.InMemory/InMemoryUserProvider.cs
@@ -0,0 +1,23 @@
+﻿using Sidekick.Training.Model;
+
+namespace Sidekick.Training.Providers.InMemory
+{
+    public class InMemoryUserProvider : IUserProvider
+    {
+        private static Dictionary<int, User> _inMemoryUser = new Dictionary<int, User>();
+        private static int _currentId = 1;
+
+        public async Task<User> CreateUser(User user)
+        {
+            user.Id = _currentId;
+            _inMemoryUser.Add(_currentId, user);
+            return user;
+            
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return _inMemoryUser.ContainsKey(id) ? _inMemoryUser[id] : null;
+        }
+    }
+}
---
+++ src/Sidekick.Training.Providers/IUserProvider.cs
@@ -0,0 +1,15 @@
+﻿using Sidekick.Training.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidekick.Training.Providers
+{
+    public interface IUserProvider
+    {
+        Task<User> CreateUser(User user);
+        Task<User> GetUserById(int id);
+    }
+}
